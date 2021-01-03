af241a7782bf3e5b8c4115e2ab77352af80877e9
Ramon Barros <pbarrosramon@gmail.com>
2021-01-03 01:13:44 -0300

    add music table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './topbar/topbar.component';
+import { MusicTableComponent } from './music-table/music-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent
+    TopbarComponent,
+    MusicTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/music-table/music-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-music-table',
+  templateUrl: './music-table.component.html',
+  styleUrls: ['./music-table.component.css']
+})
+export class MusicTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
