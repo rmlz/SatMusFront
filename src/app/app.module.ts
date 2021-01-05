a5ff4b88731ceace93c0bfa14e6e5b83cb55c871
Ramon Barros <pbarrosramon@gmail.com>
2021-01-05 05:55:41 -0300

    add login and refactored components dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { AngularFireModule } from "angularfire2";
+import { AngularFireAuth } from "angularfire2/auth";
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -6,21 +9,42 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './topbar/topbar.component';
 import { MusicsService} from '../app/musics.service';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component'
+import { AuthGuard } from "./guards/auth.guard";
+import { LoginService } from "./login.services";
+import { MidbarComponent } from './midbar/midbar.component';
+import { LowbarComponent } from './lowbar/lowbar.component';
+import { MainComponent } from './main/main.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCwrs-Qty8lb8sLhavq_ko_bP8-rh4g_xk",
+  authDomain: "satmusic-2f56e.firebaseapp.com",
+  databaseURL: "https://satmusic-2f56e-default-rtdb.firebaseio.com",
+  projectId: "satmusic-2f56e",
+  storageBucket: "satmusic-2f56e.appspot.com",
+  messagingSenderId: "858443734630",
+};
 
 @NgModule({
   declarations: [
     AppComponent,
     TopbarComponent,
+    LoginComponent,
+    MidbarComponent,
+    LowbarComponent,
+    MainComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
-  providers: [HttpClientModule, MusicsService],
+  providers: [HttpClientModule, MusicsService, AuthGuard, LoginService, 
+    AngularFireModule, AngularFireAuth],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
